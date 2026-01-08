74405800fad7662984be17dc2941dfd97bd710ed
Martin Asquino <martin.asquino@gmail.com>
2026-01-08 21:18:23 +0000

    Add earth precon sites

---
+++ src/lib/card/beta/rustic_village.rs
@@ -0,0 +1,74 @@
+
+use crate::{
+    card::{Card, CardBase, Edition, Plane, Rarity, Site, SiteBase, Zone},
+    effect::Effect,
+    game::{PlayerId, Thresholds, pick_card},
+    state::State,
+};
+
+#[derive(Debug, Clone)]
+pub struct RusticVillage {
+    pub site_base: SiteBase,
+    pub card_base: CardBase,
+}
+
+impl RusticVillage {
+    pub const NAME: &'static str = "Rustic Village";
+
+    pub fn new(owner_id: PlayerId) -> Self {
+        Self {
+            site_base: SiteBase {
+                provided_mana: 1,
+                provided_thresholds: Thresholds::parse("E"),
+                types: vec![],
+                ..Default::default()
+            },
+            card_base: CardBase {
+                id: uuid::Uuid::new_v4(),
+                owner_id,
+                tapped: false,
+                zone: Zone::Atlasbook,
+                mana_cost: 0,
+                required_thresholds: Thresholds::new(),
+                plane: Plane::Surface,
+                rarity: Rarity::Ordinary,
+                edition: Edition::Beta,
+                controller_id: owner_id.clone(),
+            },
+        }
+    }
+}
+
+impl Site for RusticVillage {}
+
+#[async_trait::async_trait]
+impl Card for RusticVillage {
+    fn get_name(&self) -> &str {
+        Self::NAME
+    }
+
+    fn get_base_mut(&mut self) -> &mut CardBase {
+        &mut self.card_base
+    }
+
+    fn get_base(&self) -> &CardBase {
+        &self.card_base
+    }
+
+    fn get_site_base(&self) -> Option<&SiteBase> {
+        Some(&self.site_base)
+    }
+
+    fn get_site_base_mut(&mut self) -> Option<&mut SiteBase> {
+        Some(&mut self.site_base)
+    }
+
+    fn get_site(&self) -> Option<&dyn Site> {
+        Some(self)
+    }
+}
+
+#[linkme::distributed_slice(crate::card::ALL_CARDS)]
+static CONSTRUCTOR: (&'static str, fn(PlayerId) -> Box<dyn Card>) = (RusticVillage::NAME, |owner_id: PlayerId| {
+    Box::new(RusticVillage::new(owner_id))
+});
