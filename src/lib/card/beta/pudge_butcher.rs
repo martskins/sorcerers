cf5d9d87fdd448e878560f822d904d69ceaa27f3
Martin Asquino <martin.asquino@gmail.com>
2026-01-10 23:22:25 +0000

    Implement pudge butcher

--- src/lib/card/beta/pudge_butcher.rs
+++ src/lib/card/beta/pudge_butcher.rs
@@ -1,7 +1,103 @@
 use crate::{
-    card::{Ability, Card, CardBase, Cost, Edition, MinionType, Plane, Rarity, UnitBase, Zone},
-    game::PlayerId,
+    card::{Ability, AdditionalCost, Card, CardBase, Cost, Edition, MinionType, Plane, Rarity, UnitBase, Zone},
+    effect::Effect,
+    game::{ActivatedAbility, BaseOption, CARDINAL_DIRECTIONS, PlayerId, pick_direction, pick_option},
+    query::{CardQuery, ZoneQuery},
+    state::State,
 };
+
+#[derive(Debug, Clone)]
+struct ShootProjectile;
+
+#[async_trait::async_trait]
+impl ActivatedAbility for ShootProjectile {
+    fn get_name(&self) -> &str {
+        "Tap to shoot projectile"
+    }
+
+    async fn on_select(
+        &self,
+        card_id: &uuid::Uuid,
+        player_id: &PlayerId,
+        state: &State,
+    ) -> anyhow::Result<Vec<Effect>> {
+        let direction = pick_direction(
+            player_id,
+            &CARDINAL_DIRECTIONS,
+            state,
+            "Pudge Butcher: Choose a direction to shoot the projectile in",
+        )
+        .await?;
+
+        let mut snapshot = state.snapshot();
+        let pudge = state.get_card(card_id);
+        let effect = Effect::ShootProjectile {
+            player_id: player_id.clone(),
+            shooter: card_id.clone(),
+            from_zone: pudge.get_zone().clone(),
+            direction,
+            damage: 0,
+            piercing: false,
+            splash_damage: None,
+        };
+        effect.apply(&mut snapshot).await?;
+        let mut target = None;
+        for effect in snapshot.effects {
+            match *effect {
+                Effect::TakeDamage {
+                    card_id: target_id,
+                    from,
+                    damage: 0,
+                    ..
+                } if &from == card_id => {
+                    target = Some(target_id.clone());
+                    break;
+                }
+                _ => {}
+            }
+        }
+
+        let mut effects = vec![effect];
+        if let Some(target) = target {
+            effects.push(Effect::MoveCard {
+                card_id: target.clone(),
+                player_id: player_id.clone(),
+                from: state.get_card(&target).get_zone().clone(),
+                to: ZoneQuery::Specific {
+                    id: uuid::Uuid::new_v4(),
+                    zone: pudge.get_zone().clone(),
+                },
+                tap: false,
+                plane: pudge.get_plane(state).clone(),
+                through_path: None,
+            });
+            let options = vec![BaseOption::Yes, BaseOption::No];
+            let option_labels: Vec<String> = options.iter().map(|o| o.to_string()).collect();
+            let picked_option = pick_option(player_id, &option_labels, state, "Pudge Butcher: Fight unit?").await?;
+            if options[picked_option] == BaseOption::Yes {
+                effects.push(Effect::Attack {
+                    attacker_id: card_id.clone(),
+                    defender_id: target,
+                });
+            }
+        }
+
+        effects.reverse();
+        Ok(effects)
+    }
+
+    fn get_cost(&self, card_id: &uuid::Uuid, _state: &State) -> anyhow::Result<Cost> {
+        Ok(Cost {
+            additional: vec![AdditionalCost::Tap {
+                card: CardQuery::Specific {
+                    id: uuid::Uuid::new_v4(),
+                    card_id: card_id.clone(),
+                },
+            }],
+            ..Default::default()
+        })
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct PudgeButcher {
@@ -56,6 +152,10 @@ impl Card for PudgeButcher {
 
     fn get_unit_base_mut(&mut self) -> Option<&mut UnitBase> {
         Some(&mut self.unit_base)
+    }
+
+    fn get_activated_abilities(&self, _state: &State) -> anyhow::Result<Vec<Box<dyn ActivatedAbility>>> {
+        Ok(vec![Box::new(ShootProjectile)])
     }
 }
 
